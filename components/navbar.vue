6e4d330bc9f518ecdf4c406dbd9e783392fd1c5a
Grayson Gantek <graysonleegantek@gmail.com>
2021-05-07 18:33:20 -0400

    adds SCSS support for global styles and component styles

---
+++ components/navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="navbar">Navbar</nav>
+</template>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/abstracts/color';
+
+.navbar {
+  background-color: custom-color('blue');
+  padding: 2rem;
+}
+</style>
