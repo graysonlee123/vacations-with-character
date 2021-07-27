3a1b1a495282de7f734b8efc991dab821295e4b4
Grayson Gantek <graysonleegantek@gmail.com>
2021-07-26 22:06:19 -0400

    adds tickets page intro

--- pages/tickets.vue
+++ pages/tickets.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <h1>Tickets</h1>
+    <Hero link="/quote">
+      <template #title>Tickets</template>
+      <template #subtitle
+        >Our tickets are secure, authorized, and come at no additional cost. Let
+        us handle the hassle of purchasing your park tickets.</template
+      >
+      <template #link>Request a Quote</template>
+    </Hero>
+    <Notice>
+      Disney, Universal Orlando, Sea World, Discovery Cove, Busch Gardens,
+      Legoland Florida
+    </Notice>
   </div>
 </template>
-
-<script>
-export default {
-  layout: 'under-construction',
-}
-</script>
